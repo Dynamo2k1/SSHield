4495c2a7a07a16b38549bf87ffccf5975c989583
dynamo2k1 <dynamo89247@email.com>
2025-02-15 12:18:49 -0500

    Update main code

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import sys
+import getpass
 import paramiko
 import threading
 import subprocess
+
+
+def ssh_command(ip,user,passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip,username=user,password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(4096))
+
+    return
+
+ssh_command('192.168.100.134','dynamo','1590','id')
 
